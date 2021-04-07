2f0cdfadb86f44da5d4999b210788749197172e4
Shanto <rakibulhasansanto25@gmail.com>
2021-04-07 23:53:05 +0600

    COMMENT THE CODE AND START THE MAP  FUNCTION

--- src/index.js
+++ src/index.js
@@ -1,58 +1,80 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import reactDom from 'react-dom';
 
 //add css
 import './index.css'
 
-const firstBook ={
-  img:'https://m.media-amazon.com/images/I/81NI6sqvvkL._AC_UY218_.jpg',
-  title:'A Promised Land',
-  author:'by Barack Obama and Random House Audio',
-}
-const secondBook ={
-  img:'https://m.media-amazon.com/images/I/81PNeyIYVfL._AC_UY218_.jpg',
-  title:'Greenlights',
-  author:'by Matthew McConaughey and Random House Audio',
-}
-const thirdBook ={
-  img:'https://m.media-amazon.com/images/I/91AQs6qv9ML._AC_UY218_.jpg',
-  title:'Untamed',
-  author:'by Glennon Doyle and Glennon Doyle Melton',
-}
+//====SOLVE THIS AMAJON BOOK LIST PROJECT USING OBJECT A)firstBook B)secondBook=======
+// const firstBook ={
+//   img:'https://m.media-amazon.com/images/I/81NI6sqvvkL._AC_UY218_.jpg',
+//   title:'A Promised Land',
+//   author:'by Barack Obama and Random House Audio',
+// }
+// const secondBook ={
+//   img:'https://m.media-amazon.com/images/I/81PNeyIYVfL._AC_UY218_.jpg',
+//   title:'Greenlights',
+//   author:'by Matthew McConaughey and Random House Audio',
+// }
+// const thirdBook ={
+//   img:'https://m.media-amazon.com/images/I/91AQs6qv9ML._AC_UY218_.jpg',
+//   title:'Untamed',
+//   author:'by Glennon Doyle and Glennon Doyle Melton',
+// }
 
-function BookList (){
-  return (
-    <section className="booklist">
-      <Book 
-      img={firstBook.img}
-      title={firstBook.title}
-      author={firstBook.author}
-      />
-      <Book 
-      img={secondBook.img}
-      title={secondBook.title}
-      author={secondBook.author}
-      />
-      <Book 
-      img={thirdBook.img}
-      title={thirdBook.title}
-      author={thirdBook.author}
-      />
-      <Book /><Book /><Book />
+// function BookList (){
+//   return (
+//     <section className="booklist">
+//       <Book 
+//       img={firstBook.img}
+//       title={firstBook.title}
+//       author={firstBook.author}
+//       />
+//       <Book 
+//       img={secondBook.img}
+//       title={secondBook.title}
+//       author={secondBook.author}
+//       />
+//       <Book 
+//       img={thirdBook.img}
+//       title={thirdBook.title}
+//       author={thirdBook.author}
+//       />
+//       <Book /><Book /><Book />
       
+//     </section>
+//   );
+// }
+
+// const Book = (props) =>{
+//  const {img,title,author} = props;
+//   return(
+//   <article className='book'>
+//     <img src={img} />
+//     <h1>{title}</h1>
+//     <h3>{author}</h3>
+//   </article>
+//   );
+// }
+
+// ReactDOM.render(<BookList />, document.getElementById("root"));
+
+
+//===NOW WE SOLVE THIS PROJECT USING MAP METHODE THAT'S IS VERY EFICIENT AND MODERN CODE STYLE
+
+
+
+function Booklist() {
+  return (
+    <section>
+      <Book />
     </section>
   );
 }
 
-const Book = (props) =>{
- const {img,title,author} = props;
+const Book = () =>{
   return(
-  <article className='book'>
-    <img src={img} />
-    <h1>{title}</h1>
-    <h3>{author}</h3>
-  </article>
+    <h1>Hi</h1>
   );
 }
 
-ReactDOM.render(<BookList />, document.getElementById("root"));
+reactDom.render(<Booklist />,document.getElementById("root"));
